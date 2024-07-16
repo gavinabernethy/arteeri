Fixed calculation of complexity dimension / scaling.

system_state.py
@@ -1106,9 +1106,10 @@ class System_state:
                 else:
                     lm_sar = {"is_success": 0}
 
-                if not (binary_complexity == 0).any():
-                    bin_comp_y_val = np.log(binary_complexity)
-                    lm_binary_complexity = linear_model_report(x_val=x_val, y_val=bin_comp_y_val,
+                # ignore first entry (patch size 0 - useful for diversity, but meaningless for intra-cluster complexity)
+                if not (binary_complexity[1:] == 0).any():
+                    bin_comp_y_val = np.log(binary_complexity[1:])
+                    lm_binary_complexity = linear_model_report(x_val=x_val[1:], y_val=bin_comp_y_val,
                                                                is_record_vectors=is_record_lm_vectors,
                                                                model_type_str="log-log")
                     # complexity dimension is -gradient
@@ -1116,9 +1117,9 @@ class System_state:
                 else:
                     lm_binary_complexity = {"is_success": 0}
 
-                if not (population_weighted_complexity == 0).any():
-                    pop_weight_y_val = np.log(population_weighted_complexity)
-                    lm_pop_weighted_complexity = linear_model_report(x_val=x_val, y_val=pop_weight_y_val,
+                if not (population_weighted_complexity[1:] == 0).any():
+                    pop_weight_y_val = np.log(population_weighted_complexity[1:])
+                    lm_pop_weighted_complexity = linear_model_report(x_val=x_val[1:], y_val=pop_weight_y_val,
                                                                      is_record_vectors=is_record_lm_vectors,
                                                                      model_type_str="log-log")
                     # complexity dimension is -gradient
@@ -1128,14 +1129,14 @@ class System_state:
 
                 # record
                 complexity_report[network_key] = {
-                    "is_success": 1,
+                    "is_cluster_success": 1,
                     "lm_sar": lm_sar,
                     "lm_binary_complexity": lm_binary_complexity,
                     "lm_pop_weighted_complexity": lm_pop_weighted_complexity,
                 }
             else:
                 complexity_report[network_key] = {
-                    "is_success": 0,
+                    "is_cluster_success": 0,
                     "lm_sar": {},
                     "lm_binary_complexity": {},
                     "lm_pop_weighted_complexity": {},

system_state_functions.py
@@ -97,16 +97,18 @@ def determine_complexity(sub_network, cluster, is_normalised):
     total_difference = 0
     if sub_network["num_patches"] > 1:
         for lower_patch_index in range(len(cluster) - 1):
+            lower_patch_num = cluster[lower_patch_index]
             for upper_patch_index in range(lower_patch_index + 1, len(cluster)):
+                upper_patch_num = cluster[upper_patch_index]
                 # compare pair
                 if is_normalised:
                     # for populations - difference should be |x_i - x_j| / bar{x}
-                    lower_vector = sub_network["normalised_population_arrays"][0][lower_patch_index, :]
-                    upper_vector = sub_network["normalised_population_arrays"][0][lower_patch_index, :]
+                    lower_vector = sub_network["normalised_population_arrays"][0][lower_patch_num, :]
+                    upper_vector = sub_network["normalised_population_arrays"][0][upper_patch_num, :]
                 else:
                     # for binary (occupancy) comparison - difference should be 1 or 0
-                    lower_vector = sub_network["population_arrays"][0][lower_patch_index, :]
-                    upper_vector = sub_network["population_arrays"][0][lower_patch_index, :]
+                    lower_vector = sub_network["population_arrays"][0][lower_patch_num, :]
+                    upper_vector = sub_network["population_arrays"][0][upper_patch_num, :]
                 difference = 0
                 for species_index in range(len(lower_vector)):
                     difference += np.abs(lower_vector[species_index] - upper_vector[species_index])